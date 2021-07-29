b803f68df0edd3b5c49567ed1e5a1c196864315d
jsm-txt <hose153@gmail.com>
2021-07-29 16:36:12 -0500

    More UI components

--- game/main/routes.py
+++ game/main/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for,json
 from game import db
 from bson.objectid import ObjectId
 import random
@@ -12,6 +12,12 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def landingPage():
     """Return our landing page to the user."""
-    return render_template("home.html")
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "data", "games.json")
+    data = json.load(open(json_url))
+    print(data)
+   
+
+    return render_template("home.html", data=data)
 
 
